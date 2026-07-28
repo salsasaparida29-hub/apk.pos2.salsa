5860c22c0b6f8eb4c1ee3ba38c0ef87c09aea017
Hype Flex X3 <dhellaramelya0@gmail.com>
2026-07-28 14:21:03 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -9,36 +9,28 @@ use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\UserController;
 
 Route::middleware('guest')->group(function () {
-
     Route::get('/login', [AuthController::class, 'index'])->name('login');
     Route::post('/auth', [AuthController::class, 'auth'])->name('auth');
-
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function {
+    Route::get('/dashaboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [DashboardController::class, 'logout'])->name('logout');
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    // Group admin (users + produk)
-    Route::middleware('role:admin,kasir')->group(function () {
-
-        // Users
-        Route::get('/users', [UserController::class, 'index'])->name('users');
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::middleware('role:admin')->prefix('admin')->name('admin.')->group(function () {
+      Route::get('/users', [UserController::class, 'index'])->name('users');
+       Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
         Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users.edit');
-        Route::post('/users/update/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/destroy/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::get('/users/edit/{users}', [UserController::class, 'edit'])->name('users.edit');
+         Route::post('/users/update/{users}', [UserController::class, 'update'])->name('users.update');
+          Route::delete('/users/destroy/{users}', [UserController::class, 'destroy'])->name('users.destroy');
 
     });
 
     Route::middleware('role:admin,kasir')->group(function () {
-
         Route::resource('/produk', ProdukController::class);
-
         Route::resource('/penjualan', PenjualanController::class);
-        Route::resource('/itempenjualan', ItemPenjualanController::class);
+        Route::resource('/itempenjualan',ItemPenjualanController::class);
     });
 
 });
